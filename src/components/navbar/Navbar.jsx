b700b70d15e1fdb6e6ba131f5d722bf2ebc1282b
coffmanjrp <coffmanjrp@gmail.com>
2021-02-25 16:54:42 -0800

    Import react icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <>
       <div className="navbar">
         <Link to="#" className="menu-bar">
-          <FaBars />
+          <FaBars onClick={showSidebar} />
         </Link>
       </div>
+      <nav className={sidebar ? 'nav-menu active' : 'nav-bar'}>
+        <ul className="nav-menu-items">
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+              <FaTimes />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 };
